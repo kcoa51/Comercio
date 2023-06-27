653698026b97aee98fc5a09b3ef8238a079efb2b
Karen Cristiny <okarencristiny0210@gmail.com>
2023-06-27 08:25:22 -0300

    commit inicial

--- src/main/java/com/itb/inf2gm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2gm/comercio/model/Produto.java
@@ -13,6 +13,50 @@ public class Produto {
 	// da própria classe.
 	// Entende-se por membros (atributos e ou métodos)
 	
+	// Métodos Setter's e Getter são responsáveis em atribuir e recuperar informações
+	// dos atributos "RESPECTIVAMENTE"
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	 
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getCodigoBarra() {
+		return codigoBarra;
+	}
+
+	public void setCodigoBarra(String codigoBarra) {
+		this.codigoBarra = codigoBarra;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
 	
 	
 }
